9d89a2a56485d9de0d12f0333d2bb422ec885eb5
erickimkk <142363912+erickimkk@users.noreply.github.com>
2026-02-01 13:33:41 -0800

    Update ex27.py

--- ex27.py
+++ ex27.py
@@ -1,1 +1,12 @@
-print(ola)
+# tuplas
+#tuplas sao imutaveis
+lanche = ('Hamburguer','Suco','Pizza','Pudim')
+for i in range(0,len(lanche)):
+    print(lanche[i])
+
+#os dois sao a mesma coisa
+
+for pos,i in enumerate(lanche):
+    print(f'vou comer {i} na posicao {pos}')
+
+
